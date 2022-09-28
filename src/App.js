a84490238048a0581560dfab5f847ce8d2b34d0c
Katariina Vakkuri <katariina.vakkuri@polarsquad.com>
2022-09-28 11:30:18 +0300

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [results, setResults] = useState({});
+
+  const fetchData = () => {
+    fetch('https://opentdb.com/api.php?amount=1')
+    .then(response => response.json())
+    .then(resData => setResults(resData))
+    .catch(err => console.log(err))
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tbody>
+        {
+          results.results.map((result, index) => 
+           <tr key={index}>
+             <td>{result.question}</td>
+           </tr>
+          )
+        }
+        </tbody>
+        </table>
+        <button onClick={fetchData}>New question</button>
     </div>
   );
 }
+
 
 export default App;
